c8fba3c0f7fbb0b64215c39fc4db44ccbd484eef
ravipramoth <ravipramoth@gmail.com>
2023-11-22 18:32:59 +0530

    changes made in ansible and terraform

--- terraform_code/vpc/ec2instancecreation.tf
+++ terraform_code/vpc/ec2instancecreation.tf
@@ -124,9 +124,9 @@ resource "aws_route_table" "dpp-public-rt" {
     vpc_id     =     aws_vpc.dpp-vpc.id
  }
 
-#   module "eks" {
-#        source = "../eks"
-#        vpc_id     =     aws_vpc.dpp-vpc.id
-#        subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
-#        sg_ids = module.sgs.security_group_public
-#  }
+  module "eks" {
+       source = "../eks"
+       vpc_id     =     aws_vpc.dpp-vpc.id
+       subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+       sg_ids = module.sgs.security_group_public
+ }
